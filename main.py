2378249e425646840959f7bd08bab3a1de151c5f
abednego emonena <emonenaabednego@gmail.com>
2024-11-04 15:54:42 +0100

    Refactor code to enable sending Telegram messages

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ async def run_bot():
                 # Send signal to Telegram
                 print(bot.signal_toString(signal))
                 print("============================")
-                #await send_message(Config.TELEGRAM_BOT_TOKEN, bot.signal_toString(signal))
+                await send_message(Config.TELEGRAM_BOT_TOKEN, bot.signal_toString(signal))
                 #await send_telegram_message(Config.TELEGRAM_BOT_TOKEN, Config.TELEGRAM_CHANNEL_ID, bot.signal_toString(signal))
                 
                 logging.info("Signal: %s", bot.signal_toString(signal))
