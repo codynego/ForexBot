388cdd0952b4a8e0f54b332f781fc091b01b28b1
abednego emonena <emonenaabednego@gmail.com>
2025-12-14 19:03:03 +0100

    Update ALLOWED_HOSTS in settings.py for deployment configuration

--- fxbot/settings.py
+++ fxbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+1sek+kxku)7f-c+-hs^ankge7x%=9wsvlq3b7ge&kmo1edxwi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cody-forexbot.koyeb.app','forexbot-1.onrender.com','10.4.1.209','gigantic-darsey-codynego-84fef88b.koyeb.app']
+ALLOWED_HOSTS = ['cody-forexbot.koyeb.app','forexbot-1.onrender.com','10.4.1.209','accepted-dela-codynego-011073d4.koyeb.app']
 
 
 # Application definition
